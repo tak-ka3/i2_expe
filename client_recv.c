7ecac7cd5641f69e0b29b57daec85d38e7abf3c8
tak-ka3 <hi.ta9.23.333@gmail.com>
2021-05-27 14:21:02 +0900

    first commit

---
+++ client_recv.c
@@ -0,0 +1,27 @@
+#include <netinet/in.h>
+#include <netinet/ip.h>
+#include <netinet/tcp.h>
+#include <stdio.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <sys/types.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char* argv[]){
+	int s = socket(PF_INET, SOCK_STREAM, 0);
+	char* ip_add = argv[1];
+	char* port = argv[2];
+	struct sockaddr_in addr;
+	addr.sin_family = AF_INET;
+	addr.sin_addr.s_addr = inet_addr(ip_add);
+	addr.sin_port = htons(atoi(port));
+	int ret = connect(s,  (struct sockaddr *) &addr, sizeof(addr));
+
+	char data[100];
+	int N = 100;
+	int n = send(s, data, N, 0);
+
+	// int n2 = recv(s, data, N, 0);
+	shutdown(s, SHUT_WR);
+}
